ceef1f057bee9512c436718a042646fbce137bc4
Petri Makijarvi <petri38-github@yahoo.com>
2019-11-05 22:07:16 +0100

    udp for signalk out

--- docs/signalk/img/mogrify.ps1
+++ docs/signalk/img/mogrify.ps1
@@ -1,4 +1,4 @@
 ﻿# A PowerShell Script to resize all *.PNG images in the directory, copy to ..
  # mogrify-command is used, from ImageMagick-package
 
-mogrify -path .. -resize '600x800>' *.png
+mogrify -path .. -resize '480x600>' *.png
