2739ec22d4bdd6d95c21389a85165440804085d6
Petri Makijarvi <petri38-github@yahoo.com>
2019-11-28 21:14:53 +0100

    increased image size in preview

--- docs/developers/img/mogrify.ps1
+++ docs/developers/img/mogrify.ps1
@@ -1,4 +1,4 @@
 ﻿# A PowerShell Script to resize all *.PNG images in the directory, copy to ..
  # mogrify-command is used, from ImageMagick-package
 
-mogrify -path .. -resize '800x600>' *.png
+mogrify -path .. -resize '1200x800>' *.png
