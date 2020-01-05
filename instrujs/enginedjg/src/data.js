541a0952890edcc0bf24bdd558183619cbe1973a
Petri Makijarvi <petri38-github@yahoo.com>
2020-01-05 13:36:55 +0100

    New method to clear data (for configuration change)

--- instrujs/enginedjg/src/data.js
+++ instrujs/enginedjg/src/data.js
@@ -9,6 +9,11 @@
 export function showData( that ) {
     that.glastvalue = window.iface.getdata()
     if ( (that.gauge.length > 0) && (that.glastvalue != null) )
-        that.gauge[0].refresh( that.glastvalue)
+        that.gauge[0].refresh( that.glastvalue )
 }
 
+export function clearData( that ) {
+    that.glastvalue = 0
+    if ( (that.gauge.length > 0) && (that.glastvalue != null) )
+        that.gauge[0].refresh( that.glastvalue )
+}
