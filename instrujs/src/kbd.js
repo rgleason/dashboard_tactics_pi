0b61fd070fe0810760a5a953a2fe5978075fe235
Petri Makijarvi <petri38-github@yahoo.com>
2020-01-16 23:20:15 +0100

    Create common kbd.js to deal with keyboard events

---
+++ instrujs/src/kbd.js
@@ -0,0 +1,62 @@
+/* $Id: kbd.js, v1.0 2019/11/30 VaderDarth Exp $
+ * OpenCPN dashboard_tactics plug-in
+ * Licensed under MIT - see distribution.
+ */
+
+// An actor for a state machine to deal with keyboard events
+
+var dbglevel = window.instrustat.debuglevel
+
+var controlKey = false
+var arrowUpKey = false
+var arrowDownKey = false
+var notBothCtrlUp = true
+var notBothCtrlDown = true
+
+window.addEventListener('keydown', function(e) {
+    if (event.defaultPrevented)
+        return
+    if ( e.ctrlKey )
+        controlKey = true
+    if ( (e.key == 'Up') || (e.key == 'ArrowUp') )
+        arrowUpKey = true
+    if ( (e.key == 'Down') || (e.key == 'ArrowDown') )
+        arrowDownKey = true
+    if ( notBothCtrlUp ) {
+        if ( controlKey && arrowUpKey ) {
+            if ( dbglevel > 2 )
+                console.log('kbd.js: Ctrl+Up')
+            // fire the event
+            notBothCtrlUp = false
+        }
+    }
+    if ( notBothCtrlDown ) {
+        if ( controlKey && arrowDownKey ) {
+            if ( dbglevel > 2 )
+                console.log('kbd.js: Ctrl+Dow')
+            // fire the event
+            notBothCtrlDown = false
+        }
+    }
+});
+
+window.addEventListener('keyup', function(e) {
+    if (event.defaultPrevented)
+        return
+    if ( !e.ctrlKey )
+        controlKey = false
+    if ( (e.key == 'Up') || (e.key == 'ArrowUp') )
+        arrowUpKey = false
+    if ( (e.key == 'Down') || (e.key == 'ArrowDown') )
+        arrowDownKey = false
+    if ( !notBothCtrlUp ) {
+        if ( !controlKey || !arrowUpKey )
+            notBothCtrlUp = true
+    }
+    if ( !notBothCtrlDown ) {
+        if ( !controlKey || !arrowDownKey )
+            notBothCtrlDown = true
+    }
+});
+
+
