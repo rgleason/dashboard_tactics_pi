39d1ed71cc425a989de624e5af16b3a9a0e65e05
Petri Mäkijärvi <petri38-github@yahoo.com>
2020-01-22 18:58:04 +0100

    Own modules for checkConf and upgradeConfVersion

--- instrujs/src/conf.js
+++ instrujs/src/conf.js
@@ -6,7 +6,9 @@
 //An actor to load, find and store configuration
 var alertsenabled = window.instrustat.alerts
 var dbglevel = window.instrustat.debuglevel
-import { loadConf, saveConf } from './persistence'
+import loadConf from './persistence'
+import saveConf from './persistence'
+import checkConf from './confvalid'
 
 // Polyfills, for IE back-end (!) on Windows used by WebView
 if (!Object.entries)
@@ -120,122 +122,4 @@ export function prepareConfHalt ( that ) {
         return
     }
     saveConf( that.uid, that.conf )
-}
-
-function checkConf ( loadedConf, refConf ) {
-    if ( loadedConf == null ) {
-        if ( dbglevel > 1 ) console.log('checkConf(): inspected object is null, returning null')
-        return null
-    }
-    if ( refConf == null ) {
-        if ( dbglevel > 1 ) console.log('checkConf(): reference object is null, returning null')
-        return null
-    }
-    // Inspect the returned object against incompatibility or older version number
-    try {
-        var refConfKeys    = Object.keys( refConf )
-        var loadedConfKeys = Object.keys( loadedConf )
-        if ( refConfKeys.length != loadedConfKeys ) {
-            var loadedConfVersion = 0
-            try {
-                if ( dbglevel > 2 )
-                    console.log('checkConf(): searching for a version number:')
-                Object.entries( loadedConf ).forEach( function( key, value ) {
-                    if ( dbglevel > 2 )
-                        console.log('checkConf(): key ', key, ' key.length ', key.length, ' value ', value)
-                    if ( key.length > 1 ) {
-                        if ( key[0] == 'version' ) {
-                            var vers = key[1]
-                            if ( dbglevel > 2 )
-                                console.log('checkConf(): vers ', vers)
-                            if ( typeof vers == 'number' )
-                                if ( dbglevel > 2 )
-                                    console.log('checkConf(): vers is a typeof "number"')
-                            if ( (key[1] != null) && (typeof key[1] == 'number') ) {
-                                if ( dbglevel > 2 )
-                                console.log('checkConf(): version key and number value found')
-                                loadedConfVersion = key[1]
-                            }
-                        }
-                    }
-                })
-            }
-            catch ( error ) {
-                if ( dbglevel > 1 )
-                    console.log(
-                        'checkConf(): exception while inspecting the object (entries), error: ', error)
-            }
-            if ( loadedConfVersion == 0 ) {
-                if ( dbglevel > 1 )
-                    console.log('checkConf(): the object has unkwon or zero version number - ignoring!')
-                return nujll
-            }
-            else if ( loadedConfVersion < refConf.version ) {
-                if ( dbglevel > 1 )
-                    console.log('checkConf(): Object with lower version number, ', loadedConfVersion,
-                                ' compared to current version number, ', refConf.version, ' - upgrading.')
-                loadedConf = upgradeConfVersion( loadedConfVersion, loadedConf )
-                if ( loadedConf == null ) {
-                    if ( dbglevel > 1 )
-                        console.log('checkConf(): upgradeConfVersion() fails in upgrade, returns null, ignoring all.')
-                    return null
-                }
-            }
-            else {
-                if ( loadedConfKeys.length != refConfKeys.length ) {
-                    if ( dbglevel > 1 )
-                        console.log(
-                            'checkConf(): the object has different number of keys, ', loadedConfKeys.length,
-                            ' compared to thr reference object\'s number of keys, ', refConfKeys.length, ' - cannot continue.')
-                    if ( alertsenabled )
-                        alert ( window.instrulang.errNofConfKeysDoNotMatch1 + '\n' +
-                                window.instrulang.errNofConfKeysDoNotMatch2 + '\n' +
-                                window.instrulang.errNofConfKeysDoNotMatch3 + ' ' + refConfKeys.length, + ' ' +
-                                window.instrulang.errNofConfKeysDoNotMatch4 + ' ' + loadedConfKeys.length )
-                    return null
-                }
-            }
-        }
-    }
-    catch ( error ) {
-        if ( dbglevel > 1 )
-            console.log('checkConf(): exception while inspecting the object (keys), error: ', error)
-            return null
-    }
-
-    try {
-        if ( (loadedConf.path == null) || (loadedConf.path == '') ) {
-            if ( dbglevel > 1 )
-                console.log('checkConf(): the object does not have a path-key or it is empty.',
-                            ' Cannot continue.')
-            return null
-        }
-    }
-    catch ( error ) {
-        if ( dbglevel > 1 )
-            console.log('checkConf(): exception while inspecting the object (path), error: ', error)
-        return null
-    }
-
-    // Make a shallow comparison for the same keys
-    try {
-        if ( !areEqualShallowKeys( refConf, loadedConf ) ) {
-            if ( dbglevel > 1 )
-                console.log(
-                    'checkConf(): Objects do not have the same keys (EqualyShallow test).',
-                    ' Cannot continue.')
-            return null
-        }
-    }
-    catch ( error ) {
-        if ( dbglevel > 1 )
-            console.log(
-                'checkConf(): exception while inspecting the object (EqualyShallow - keys), error: ', error)
-        return null
-    }
-    return loadedConf
-}
-
-function upgradeConfVersion( loadecConfVersion, loadedConf ) {
-    return null
 }
