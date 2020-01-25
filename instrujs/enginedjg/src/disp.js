ccc8eae930ca0eef34dda464c8579eb888eccf12
Petri Makijarvi <petri38-github@yahoo.com>
2020-01-25 22:42:59 +0100

    Codacy

--- instrujs/enginedjg/src/disp.js
+++ instrujs/enginedjg/src/disp.js
@@ -4,34 +4,38 @@
  */
 
 // An actor to handle the display object(s)
+
+import sanitizer from '../../src/escapeHTML'
+var Sanitizer = sanitizer()
+
 import { createGauge } from './gauge'
 import { hasProportionalFontSupport } from './css'
 import { showData } from './data'
 import { memorizeSettings } from '../../src/conf'
 
 export function swapDisplay( that, direction, memorize ) {
-    var direction = direction || 'up'
+    var dir = direction || 'up'
     var memorizeChange = memorize || true
     if ( (that.gauge.length > 0) ) {
         var removedgauge = that.gauge.pop()
         removedgauge.destroy()
     }
     var nextdisp = 'dial'
-    if ( direction === 'up' ) {
-        if ( that.conf.display == 'dial' )
+    if ( dir === 'up' ) {
+        if ( that.conf.display === 'dial' )
             nextdisp = 'donut'
-        else if ( that.conf.display == 'donut' )
+        else if ( that.conf.display === 'donut' )
             nextdisp = 'simple'
     }
     else {
-        if ( that.conf.display == 'dial' )
+        if ( that.conf.display === 'dial' )
             nextdisp = 'simple'
-        else if ( that.conf.display == 'simple' )
+        else if ( that.conf.display === 'simple' )
             nextdisp = 'donut'
     }
     
-    if ( nextdisp == 'simple' ) {
-        document.getElementById('gauge0').innerHTML =
+    if ( nextdisp === 'simple' ) {
+        var htmlCandidate =
             '<div class="numgauge ' +
             (hasProportionalFontSupport?'propl ':'fixed ') +
             that.luminosity +
@@ -40,10 +44,13 @@ export function swapDisplay( that, direction, memorize ) {
             (hasProportionalFontSupport?'propl ':'fixed ') +
             that.luminosity +
             '" id="numgunit0"></div>'
+        var htmlObj = Sanitizer.createSafeHTML(htmlCandidate)
+        document.getElementById('gauge0').innerHTML =
+            Sanitizer.unwrapSafeHTML(htmlObj)
     }
     else {
         document.getElementById('gauge0').innerHTML = ''
-        if (nextdisp == 'dial') {
+        if (nextdisp === 'dial') {
             that.gauge.push (
                 createGauge('gauge0', 0, that.conf.decimals,
                             that.conf.unit, false, that.conf.symbol ) )
