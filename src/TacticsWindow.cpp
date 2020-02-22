ed7c18d62aa0f095a456ed5e4107083dc822af3b
Petri Makijarvi <petri38-github@yahoo.com>
2020-02-22 23:44:18 +0100

    Refactoring, now can use SkData directly

---
+++ src/TacticsWindow.cpp
@@ -0,0 +1,180 @@
+/***************************************************************************
+* $Id: TacticsWindow.cpp, v1.0 2019/11/30 VaderDarth Exp $
+*
+* Project:  OpenCPN
+* Purpose:  tactics Plugin
+* Author:   Thomas Rauch
+*       (Inspired by original work from Jean-Eudes Onfray)
+***************************************************************************
+*   Copyright (C) 2010 by David S. Register                               *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it underta the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
+***************************************************************************
+*/
+
+using namespace std;
+
+#include <mutex>
+
+#include "TacticsWindow.h"
+#include "tactics_pi.h"
+#include "skdata.h"
+
+extern wxString GetUUID(void);
+
+TacticsWindow::TacticsWindow (
+    wxWindow *pparent, wxWindowID id,
+    tactics_pi *tactics, const wxString derivtitle,
+    SkData *pSkData) :
+    wxWindow(
+        pparent, id, wxDefaultPosition, wxDefaultSize,
+        wxBORDER_NONE || wxFULL_REPAINT_ON_RESIZE, derivtitle )
+{
+    m_plugin = tactics;
+    m_callbacks = new callback_map();
+    std::unique_lock<std::mutex> init_m_mtxCallBackContainer( m_mtxCallBackContainer, std::defer_lock );
+    m_pSkData = pSkData;
+    return;
+}
+
+TacticsWindow::~TacticsWindow()
+{
+    delete m_callbacks;
+    return;
+}
+
+void TacticsWindow::InsertTacticsIntoContextMenu ( wxMenu *contextMenu )
+{
+    wxMenuItem* btnShowLaylines = contextMenu->AppendCheckItem(
+        ID_DASH_LAYLINE, _(L"\u2191Show Laylines"));
+    btnShowLaylines->Check(m_plugin->GetLaylineVisibility());
+
+    wxMenuItem* btnShowCurrent = contextMenu->AppendCheckItem(
+        ID_DASH_CURRENT, _(L"\u2191Show Current"));
+    btnShowCurrent->Check(m_plugin->GetCurrentVisibility());
+
+	wxMenuItem* btnShowWindbarb = contextMenu->AppendCheckItem(
+        ID_DASH_WINDBARB, _(L"\u2191Show Windbarb"));
+	btnShowWindbarb->Check(m_plugin->GetWindbarbVisibility());
+
+	wxMenuItem* btnShowPolar = contextMenu->AppendCheckItem(
+        ID_DASH_POLAR, _(L"\u2191Show Polar"));
+	btnShowPolar->Check(m_plugin->GetPolarVisibility());
+
+    return;
+}
+
+void TacticsWindow::TacticsInContextMenuAction ( const int eventId )
+{
+    bool toggled = false;
+	switch (eventId){
+	case ID_DASH_LAYLINE: {
+		m_plugin->ToggleLaylineRender();
+        toggled = true;
+        break;
+	}
+	case ID_DASH_CURRENT: {
+		m_plugin->ToggleCurrentRender();
+        toggled = true;
+        break;
+	}
+	case ID_DASH_POLAR: {
+		m_plugin->TogglePolarRender();
+        toggled = true;
+        break;
+	}
+	case ID_DASH_WINDBARB: {
+		m_plugin->ToggleWindbarbRender();
+        toggled = true;
+        break;
+	}
+	}
+    /* Port note: Parent cannot save for child,
+       child cannot save for parent. */
+    if ( toggled )
+       m_plugin->SaveConfig();
+
+	return;
+
+}
+void TacticsWindow::SendPerfSentenceToAllInstruments(
+    unsigned long long st, double value, wxString unit, long long timestamp )
+{
+    m_plugin->SendSentenceToAllInstruments( st, value, unit, timestamp );
+}
+void TacticsWindow::SetUpdateSignalK(
+        wxString *type, wxString *sentenceId, wxString *talker, wxString *src, int pgn,
+        wxString *path, double value, wxString *valStr, long long timestamp, wxString *key )
+{
+    m_plugin->SetUpdateSignalK( type, sentenceId, talker, src, pgn, path, value, valStr, timestamp, key );
+}
+
+wxString TacticsWindow::subscribeTo ( wxString path, callbackFunction callback)
+{
+    wxString retUUID = GetUUID();
+    std::string keyID = std::string( retUUID.mb_str() );
+    callbackFunctionTuple newEntry = std::make_tuple(path, callback);
+    std::unique_lock<std::mutex> lckmCallBack( m_mtxCallBackContainer );
+    m_callbacks->insert ( make_pair(keyID, newEntry) );
+    m_pSkData->UpdateSubscriptionList( &path, NULL) ;
+    return retUUID;
+}
+
+void TacticsWindow::unsubscribeFrom ( wxString callbackUUID )
+{
+    std::string keyID = std::string( callbackUUID.mb_str() );
+    std::unique_lock<std::mutex> lckmCallBack( m_mtxCallBackContainer );
+    callback_map::iterator it = m_callbacks->find( keyID );
+    if ( it != m_callbacks->end() ) {
+            m_callbacks->erase( it );
+    } // key found, delete
+}
+void TacticsWindow::SendDataToAllPathSubscribers(
+    wxString path, double value, wxString unit, long long timestamp )
+{
+    std::unique_lock<std::mutex> lckmCallBack( m_mtxCallBackContainer );
+    callback_map::iterator it = m_callbacks->begin();
+    callbackFunctionPair  thisEntry;
+    callbackFunctionTuple thisSubscriber;
+    std::string keyID;
+    wxString subscribedPath;
+    callbackFunction callThis;
+    while ( it != m_callbacks->end() ) {
+        thisEntry = *it;
+        keyID = std::get<0>(thisEntry);
+        if ( !keyID.empty() ) {
+            thisSubscriber = std::get<1>(thisEntry);
+            subscribedPath = std::get<0>(thisSubscriber);
+            if ( subscribedPath == path ) {
+                    callThis = std::get<1>(thisSubscriber);
+                    callThis ( value, unit, timestamp );
+            }
+        }
+        ++it;
+    }
+}
+wxString TacticsWindow::getAllNMEA0183JsOrderedList()
+{
+    return m_pSkData->getAllNMEA0183JsOrderedList();
+}
+wxString TacticsWindow::getAllNMEA2000JsOrderedList()
+{
+    return m_pSkData->getAllNMEA2000JsOrderedList();
+}
+void TacticsWindow::collectAllSignalKDeltaPaths()
+{
+    m_pSkData->subscribeToAllPaths();
+}
---
+++ src/TacticsWindow.h
@@ -0,0 +1,86 @@
+/******************************************************************************
+ * $Id: TacticsWindow.h, v1.0 v1.0 2019/11/30 VaderDarth Exp $
+ *
+ * Project:  OpenCPN
+ * Purpose:  Dashboard Plugin
+ * Author:   Jean-Eudes Onfray
+ *
+ ***************************************************************************
+ *   Copyright (C) 2010 by David S. Register                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
+ ***************************************************************************
+ */
+
+#ifndef _TACTICSWINDOW_H_
+#define _TACTICSWINDOW_H_
+
+#include "wx/wxprec.h"
+
+#ifndef  WX_PRECOMP
+#include "wx/wx.h"
+#endif //precompiled headers
+
+#include <unordered_map>
+#include <mutex>
+
+class tactics_pi;
+class SkData;
+
+// helpers for the call-back methods in instruments subsribing to signal paths
+typedef std::function<void  (double, wxString, long long)> callbackFunction;
+typedef std::tuple<wxString, callbackFunction> callbackFunctionTuple;
+typedef std::pair<wxString, callbackFunctionTuple> callbackFunctionPair;
+//typedef std::unordered_multimap<wxString, callbackFunctionTuple> callback_map;
+typedef std::unordered_multimap<std::string, callbackFunctionTuple> callback_map;
+
+class TacticsWindow : public wxWindow
+{
+public:
+    TacticsWindow(
+        wxWindow* pparent, wxWindowID id,
+        tactics_pi* tactics, const wxString derivtitle,
+        SkData* pSkData);
+    ~TacticsWindow();
+
+    virtual void InsertTacticsIntoContextMenu (
+        wxMenu *contextMenu ) final;
+    virtual void TacticsInContextMenuAction (
+        const int eventId ) final;
+
+    void SendPerfSentenceToAllInstruments(
+        unsigned long long st, double value, wxString unit, long long timestamp );
+    void SetUpdateSignalK(
+        wxString* type, wxString* sentenceId, wxString* talker, wxString* src, int pgn,
+        wxString* path, double value, wxString* valStr, long long timestamp, wxString* key=NULL);
+    wxString subscribeTo ( wxString path, callbackFunction callback);
+    void unsubscribeFrom ( wxString callbackUUID );
+    void SendDataToAllPathSubscribers(
+        wxString path, double value, wxString unit, long long timestamp );
+    wxString getAllNMEA0183JsOrderedList(void);
+    wxString getAllNMEA2000JsOrderedList(void);
+    void collectAllSignalKDeltaPaths(void); 
+protected:
+    std::mutex          m_mtxCallBackContainer;
+    callback_map       *m_callbacks;
+    SkData             *m_pSkData;
+
+private:
+    tactics_pi*         m_plugin;
+
+};
+
+#endif // _TACTICSWINDOW_H_
