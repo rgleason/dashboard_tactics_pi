fe2ca7721a28d4333bff6cf926ff747af2b5c6d5
Petri Makijarvi <petri38-github@yahoo.com>
2020-04-22 21:49:11 +0200

    chore: remove influxdb-client public from commits

--- instrujs/src/influxdb-client/scripts/require-yarn.js
+++
@@ -1,6 +0,0 @@
-// see `yarn run env` vs `npm run env`
-if (!/yarn\//.test(process.env.npm_config_user_agent)) {
-  throw new Error(
-    `Use yarn in place of '${process.env.npm_config_user_agent}' !`
-  )
-}
