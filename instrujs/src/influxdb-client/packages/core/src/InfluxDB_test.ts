198409363a0420706551b4b2aa58ac65c285879f
Petri Makijarvi <petri38-github@yahoo.com>
2020-04-22 22:15:59 +0200

    chore: get back the influxdb-client mod package

---
+++ instrujs/src/influxdb-client/packages/core/src/InfluxDB_test.ts
@@ -0,0 +1,8 @@
+import "core-js"
+import "regenerator-runtime/runtime.js"
+export default class InfluxDB {
+  constructor(options: string) {
+      this.name = 'kukkuu'
+  }
+ private name: string
+}
