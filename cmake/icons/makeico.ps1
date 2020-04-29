b62f82c49ba7cde54d54edb68f905d0076316d73
Petri Makijarvi <petri38-github@yahoo.com>
2020-04-29 22:53:02 +0200

    refactoring: icon building now under cmake

--- cmake/icons/makeico.ps1
+++ cmake/icons/makeico.ps1
@@ -1,4 +1,6 @@
-﻿# A PowerShell Script to build an icon file *.ico
+﻿# /* $Id: makeico.ps1, v1.0 2019/11/30 VaderDarth Exp $ */
+#
+# A PowerShell Script to build an icon file *.ico
 # convert-command is used, from ImageMagick-package
 
 convert Dashboard_Tactics_256px.png -bordercolor white -border 0 Dashboard_Tactics_128px.png Dashboard_Tactics_64px.png Dashboard_Tactics_32px.png Dashboard_Tactics_24px.png Dashboard_Tactics_16px.png -delete 0 -alpha off -colors 256 Dashboard_Tactics.ico
