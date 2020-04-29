7ad0f8ccbd7e42c994417c8391d5a2f9c1251049
Petri Makijarvi <petri38-github@yahoo.com>
2020-04-29 22:53:48 +0200

    docs: add header information

--- data/instrujs/scripts/linux/httpserver.sh
+++ data/instrujs/scripts/linux/httpserver.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+# /* $Id: httpserver.ps1, v1.0 2019/11/30 VaderDarth Exp $ */
+#
+# Launching a simple node_modules http-server on instrujs folder
+
 http-server /usr/share/opencpn/plugins/dashboard_tactics_pi/data/instrujs/ -p 8088
