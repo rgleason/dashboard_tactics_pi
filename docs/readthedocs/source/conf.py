ebdb73b9fed395abcee4e37b64b72496bfba31dc
Petri Makijarvi <petri38-github@yahoo.com>
2020-08-21 23:41:14 +0200

    docs: 6th upstream strangeness

--- docs/readthedocs/source/conf.py
+++ docs/readthedocs/source/conf.py
@@ -35,6 +35,8 @@ extensions = [
     'sphinx.ext.mathjax',  # for math equations
 ]
 
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
