9658001533b564856fbf80131ea3ee874e2e0800
Petri Makijarvi <petri38-github@yahoo.com>
2020-08-21 22:01:30 +0200

    docs: 5th upstream strangeness

--- docs/readthedocs/source/enginedjg/img/mogrify.ps1
+++ docs/readthedocs/source/enginedjg/img/mogrify.ps1
@@ -1,4 +1,4 @@
 ﻿# A PowerShell Script to resize all *.PNG images in the directory, copy to ..
  # mogrify-command is used, from ImageMagick-package
 
-mogrify -path .. -resize '480x600>' *.png
+mogrify -path ../simg -resize '480x600>' *.png
