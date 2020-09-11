1d3258c32e09682fe88c2ff06b0f7fc0487474b2
Petri Makijarvi <petri38-github@yahoo.com>
2020-09-11 20:44:50 +0200

    fix: try with default html_theme readthedocs.io

--- docs/readthedocs/source/conf.py
+++ docs/readthedocs/source/conf.py
@@ -52,7 +52,7 @@ source_suffix = ['.rst', '.ipynb']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'agogo'
+# html_theme = 'agogo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
