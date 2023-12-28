2a20d008c89ed45a35b6254cce5ffc558dc626f0
Rick Gleason <fcgleason@tgp-architects.com>
2023-12-28 18:47:52 -0500

    [skip ci]rm pygments and jsut .rst

--- docs/readthedocs/source/conf.py
+++ docs/readthedocs/source/conf.py
@@ -44,7 +44,7 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-source_suffix = ['.rst', '.ipynb']
+source_suffix = ['.rst']
 
 
 # -- Options for HTML output -------------------------------------------------
